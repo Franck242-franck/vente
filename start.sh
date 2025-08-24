8e4e4317f5fb9a7d5e0431e5647a2751f6f32f21
Franck242-franck <nkouampafranck49@gmail.com>
2025-08-24 23:15:04 +0100

    Update start.sh for migrations, collectstatic and superuser creation

--- start.sh
+++ start.sh
@@ -6,11 +6,13 @@ python manage.py migrate
 echo "=== Collecting static files ==="
 python manage.py collectstatic --noinput
 
-echo "=== Creating superuser ==="
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); \
-if not User.objects.filter(username='admin').exists(): \
-    User.objects.create_superuser('admin', 'admin@gmail.com', '6vY!zE2q#Mn4*Rw')" \
-| python manage.py shell
+echo "=== Creating superuser if not exists ==="
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'papaet242@@')
+END
 
 echo "=== Starting Django server ==="
-gunicorn gestion_produits.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn gestion_produits.wsgi:application --bind 0.0.0.0:\$PORT
