3c9d9923fff0fcebff47060f66235e26cc786287
Franck242-franck <nkouampafranck49@gmail.com>
2025-08-24 23:00:18 +0100

    Force LF line endings for start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,11 @@ python manage.py migrate
 echo "=== Collecting static files ==="
 python manage.py collectstatic --noinput
 
+echo "=== Creating superuser ==="
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); \
+if not User.objects.filter(username='admin').exists(): \
+    User.objects.create_superuser('admin', 'admin@gmail.com', '6vY!zE2q#Mn4*Rw')" \
+| python manage.py shell
+
 echo "=== Starting Django server ==="
 gunicorn gestion_produits.wsgi:application --bind 0.0.0.0:$PORT
-
-
