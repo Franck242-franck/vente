945403203536340a8317a3b9cbdaa8573e239126
Franck242-franck <nkouampafranck49@gmail.com>
2025-08-25 02:17:41 +0100

    Normalize start.sh line endings

--- start.sh
+++ start.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-
 echo "=== Applying migrations ==="
 python manage.py migrate
-
 echo "=== Collecting static files ==="
 python manage.py collectstatic --noinput
-
 echo "=== Creating superuser if not exists ==="
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); \
-if not User.objects.filter(username='admin').exists(): User.objects.create_superuser('admin', 'admin@example.com', 'papaet242@@')"
-
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'papaet242@@')
+EOF
 echo "=== Starting Django server ==="
 gunicorn gestion_produits.wsgi:application --bind 0.0.0.0:8080
